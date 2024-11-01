d63f3800c937c9f920686f694be8eef08755ddf1
milindaShehan <milinda.msd@gmail.com>
2024-11-02 02:41:00 +0530

    navbar

--- freshbowl-frontend/src/App.jsx
+++ freshbowl-frontend/src/App.jsx
@@ -1,14 +1,15 @@
 
 import './App.css'
+import NavBar from './components/NavBar'
 
 function App() {
 
 
   return (
     <>
-      <div className="text-center bg-blue-500 text-white p-4">
-      <h1 className="text-3xl font-bold bg-blue-500">Hello, Tailwind CSS!</h1>
-    </div>
+      <div>
+        <NavBar />
+      </div>
     </>
   )
 }
