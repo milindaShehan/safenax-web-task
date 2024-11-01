378bdc15b77bc1c6cea5522d303a685dd78b2e30
milindaShehan <milinda.msd@gmail.com>
2024-11-02 03:59:30 +0530

    fix: width and height

--- freshbowl-frontend/src/components/Hero.jsx
+++ freshbowl-frontend/src/components/Hero.jsx
@@ -5,8 +5,8 @@ import { Button } from '@nextui-org/react';
 const Hero = () => {
     return (
         <>
-            <div className="w-full py-5 px-5"></div>
-                <div className="relative mx-auto w-full bg-cover bg-center max-w-[1366px] rounded-2xl min-h-[70vh] flex p-10 items-center justify-start" style={{ backgroundImage: `url(${HeroImg}), linear-gradient(0deg, #D9D9D9, #D9D9D9), linear-gradient(360deg, rgba(0, 0, 255, 0.5) 0%, rgba(0, 0, 0, 0) 100%)` }}>
+            <div className="w-full  px-5"></div>
+                <div className="relative mx-auto w-full bg-cover bg-center max-w-[1366px] rounded-2xl min-h-[75vh] flex p-10 items-center justify-start" style={{ backgroundImage: `url(${HeroImg}), linear-gradient(0deg, #D9D9D9, #D9D9D9), linear-gradient(360deg, rgba(0, 0, 255, 0.5) 0%, rgba(0, 0, 0, 0) 100%)` }}>
                     <div className="rounded-2xl opacity-70 inset-0 absolute bg-gradient-to-r from-black via-transparent to-transparent"></div>
                     <div className="relative w-2/3 gap-5">
                         <h1 className="font-bold text-gray-50 text-6xl mb-5">Fuel Your Body</h1>
